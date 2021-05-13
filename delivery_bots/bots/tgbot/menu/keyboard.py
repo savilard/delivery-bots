4422ad80c8f56f1a847db0014da86b6c6d2de5a5
savilard <vitaliyantonoff@gmail.com>
2021-05-13 10:59:52 +0600

    refactor(tgbot): make create_menu_keyboard func clean

--- delivery_bots/bots/tgbot/menu/keyboard.py
+++ delivery_bots/bots/tgbot/menu/keyboard.py
@@ -1,10 +1,8 @@
+from typing import List
+
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from more_itertools import chunked
 
-from delivery_bots.api.moltin.auth.auth import get_headers
-from delivery_bots.api.moltin.catalog_products.catalog_product import (
-    fetch_catalog_products,
-)
 from delivery_bots.api.moltin.catalog_products.schemas import CatalogProduct
 
 NEXT_BUTTON = InlineKeyboardButton(
@@ -18,34 +16,26 @@ PREV_BUTTON = InlineKeyboardButton(
 )
 
 
-async def create_menu_keyboard(chunk=0) -> InlineKeyboardMarkup:
+async def create_menu_keyboard(catalog_products: List[CatalogProduct], chunk=0) -> InlineKeyboardMarkup:
     """The function collects a keyboard for main menu of telegram bot."""
-    headers = await get_headers()
-    raw_catalog_products = await fetch_catalog_products(headers)
+    chunks = list(chunked(catalog_products, 8))
 
-    if 'data' in raw_catalog_products:
-        catalog_products = [
-            CatalogProduct(**raw_catalog_product) for raw_catalog_product in raw_catalog_products.get('data')
+    keyboard = [
+        [
+            InlineKeyboardButton(
+                catalog_product.name,
+                callback_data=catalog_product.id,
+            ),
         ]
+        for catalog_product in chunks[chunk]
+        if catalog_product.status == 'live'
+    ]
 
-        chunks = list(chunked(catalog_products, 8))
+    if chunk == 0:
+        keyboard.append([NEXT_BUTTON])
+    elif chunk == len(chunks) - 1:
+        keyboard.append([PREV_BUTTON])
+    else:
+        keyboard.append([PREV_BUTTON, NEXT_BUTTON])
 
-        keyboard = [
-            [
-                InlineKeyboardButton(
-                    catalog_product.name,
-                    callback_data=catalog_product.id,
-                ),
-            ]
-            for catalog_product in chunks[chunk]
-            if catalog_product.status == 'live'
-        ]
-
-        if chunk == 0:
-            keyboard.append([NEXT_BUTTON])
-        elif chunk == len(chunks) - 1:
-            keyboard.append([PREV_BUTTON])
-        else:
-            keyboard.append([PREV_BUTTON, NEXT_BUTTON])
-
-        return InlineKeyboardMarkup(inline_keyboard=keyboard)
+    return InlineKeyboardMarkup(inline_keyboard=keyboard)
