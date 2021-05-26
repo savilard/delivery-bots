33ccbea3665cf7c055cea205b65e68367e9365fc
savilard <vitaliyantonoff@gmail.com>
2021-05-26 12:27:05 +0600

    feat(tgbot): add sending catalog product image to chat

--- delivery_bots/api/moltin/files/file_detail.py
+++ delivery_bots/api/moltin/files/file_detail.py
@@ -1,6 +1,7 @@
 import httpx
 
 from delivery_bots.api.moltin.errors.exceptions import MoltinError
+from delivery_bots.api.moltin.files.schemas import MoltinFile
 
 FILE_BASE_URL = 'https://api.moltin.com/v2/files/'
 
@@ -27,3 +28,35 @@ async def fetch_moltin_file_detail(headers: dict[str, str], file_id: str) -> htt
         except httpx.HTTPStatusError:
             raise MoltinError(response.json())  # type: ignore
         return response
+
+
+async def parse_moltin_file_detail_response(response: httpx.Response) -> MoltinFile:
+    """
+    Parse moltin file detail response.
+
+    Args:
+        response: response if fetching moltin file detail response
+
+    Returns:
+        MoltinFile
+    """
+    raw_moltin_file_detail = response.json()
+    return MoltinFile(**raw_moltin_file_detail)
+
+
+async def get_catalog_product_image_url(image_id: str, headers: dict[str, str]) -> str:
+    """Gets catalog product image id.
+
+    Args:
+        image_id: id of moltin catalog product
+        headers: headers for moltin api auth
+
+    Returns:
+        object: link to moltin catalog product image
+    """
+    file_detail_response = await fetch_moltin_file_detail(
+        headers=headers,
+        file_id=image_id,
+    )
+    file_detail = await parse_moltin_file_detail_response(response=file_detail_response)
+    return file_detail.data.link.href
--- delivery_bots/bots/tgbot/catalog_product_detail/messages.py
+++ delivery_bots/bots/tgbot/catalog_product_detail/messages.py
@@ -5,17 +5,29 @@ from delivery_bots.api.moltin.catalog_products.catalog_product import (
     fetch_catalog_product_detail,
     parse_catalog_product_detail_response,
 )
+from delivery_bots.api.moltin.files.file_detail import get_catalog_product_image_url
 
 
 async def send_detailed_catalog_product_description(query: CallbackQuery) -> None:
     """Sends message with a detailed catalog product description."""
-    catalog_product_id = query.data
     headers = await get_headers()
-    catalog_product_detail_response = await fetch_catalog_product_detail(catalog_product_id, headers)
+
+    catalog_product_detail_response = await fetch_catalog_product_detail(
+        catalog_product_id=query.data,
+        headers=headers,
+    )
     catalog_product_detail = await parse_catalog_product_detail_response(catalog_product_detail_response)
+    catalog_product_image_url = await get_catalog_product_image_url(
+        headers=headers,
+        image_id=catalog_product_detail.relationships.main_image.data.id,  # type: ignore
+    )
+
     message_text = '{name}\n Стоимость - {price}\n\n{description}'.format(
         name=catalog_product_detail.name,
         price=catalog_product_detail.meta.display_price.with_tax.formatted,
         description=catalog_product_detail.description,
     )
-    await query.message.answer(text=message_text)
+    await query.message.answer_photo(
+        photo=catalog_product_image_url,
+        caption=message_text,
+    )
