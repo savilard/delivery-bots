636652b69d61b1796185246ba5b0b6c86ce54988
savilard <vitaliyantonoff@gmail.com>
2021-05-27 09:48:39 +0600

    feat(tgbot): add "add to cart" button
    to catalog product detail

--- delivery_bots/bots/tgbot/catalog_product_detail/keyboard.py
+++ delivery_bots/bots/tgbot/catalog_product_detail/keyboard.py
@@ -1,14 +1,21 @@
-from aiogram.types import InlineKeyboardMarkup
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.utils.emoji import emojize
 
 from delivery_bots.bots.tgbot.common.keyboard_buttons import (
     CART_BUTTON,
     SHOW_MENU_BUTTON,
 )
 
+ADD_TO_CART_BUTTON = InlineKeyboardButton(
+    text=emojize('➕ Добавить в корзину'),
+    callback_data='add_to_cart',
+)
+
 
 async def create_catalog_product_detail_keyboard() -> InlineKeyboardMarkup:
     """The function collects a keyboard for product description."""
     keyboard = [
+        [ADD_TO_CART_BUTTON],
         [CART_BUTTON],
         [SHOW_MENU_BUTTON],
     ]
