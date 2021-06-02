cd2c856aa3d013f81269100e61508703cb2e1511
savilard <vitaliyantonoff@gmail.com>
2021-06-02 14:13:32 +0600

    refactor(tgbot): move cart buttons to cart module

--- delivery_bots/bots/tgbot/catalog_product_detail/keyboard.py
+++ delivery_bots/bots/tgbot/catalog_product_detail/keyboard.py
@@ -1,22 +1,20 @@
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from aiogram.utils.emoji import emojize
+from aiogram.types import InlineKeyboardMarkup
 
-from delivery_bots.bots.tgbot.common.keyboard_buttons import (
-    CART_BUTTON,
-    SHOW_MENU_BUTTON,
+from delivery_bots.bots.tgbot.cart.buttons import (
+    make_add_product_to_cart_button,
+    make_cart_button,
 )
-
-ADD_TO_CART_BUTTON = InlineKeyboardButton(
-    text=emojize('➕ Добавить в корзину'),
-    callback_data='add_to_cart',
-)
+from delivery_bots.bots.tgbot.common.keyboard_buttons import SHOW_MENU_BUTTON
 
 
 async def create_catalog_product_detail_keyboard() -> InlineKeyboardMarkup:
     """The function collects a keyboard for product description."""
+    cart_button = await make_cart_button()
+    add_product_to_cart_button = await make_add_product_to_cart_button()
+
     keyboard = [
-        [ADD_TO_CART_BUTTON],
-        [CART_BUTTON],
+        [add_product_to_cart_button],
+        [cart_button],
         [SHOW_MENU_BUTTON],
     ]
     return InlineKeyboardMarkup(inline_keyboard=keyboard)
--- delivery_bots/bots/tgbot/menu/keyboard.py
+++ delivery_bots/bots/tgbot/menu/keyboard.py
@@ -4,7 +4,7 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from more_itertools import chunked
 
 from delivery_bots.api.moltin.catalog_products.schemas import CatalogProduct
-from delivery_bots.bots.tgbot.common.keyboard_buttons import CART_BUTTON
+from delivery_bots.bots.tgbot.cart.buttons import make_cart_button
 
 NEXT_BUTTON = InlineKeyboardButton(
     text='Вперёд ➡️',
@@ -39,6 +39,6 @@ async def create_menu_keyboard(catalog_products: List[CatalogProduct], chunk=0) 
     else:
         keyboard.append([PREV_BUTTON, NEXT_BUTTON])
 
-    keyboard.append([CART_BUTTON])
+    keyboard.append([await make_cart_button()])
 
     return InlineKeyboardMarkup(inline_keyboard=keyboard)
