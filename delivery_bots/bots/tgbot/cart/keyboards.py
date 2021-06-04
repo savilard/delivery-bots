7f25c90be7df636108840c835d9a317180289d00
Vitaliy Antonov <60862933+savilard@users.noreply.github.com>
2021-06-04 10:15:25 +0600

    feat(tgbot): add transition to menu from cart (#14)

--- delivery_bots/bots/tgbot/cart/keyboards.py
+++ delivery_bots/bots/tgbot/cart/keyboards.py
@@ -1,6 +1,7 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from delivery_bots.api.moltin.cart.schemas import CartProduct
+from delivery_bots.bots.tgbot.common.keyboard_buttons import SHOW_MENU_BUTTON
 
 
 async def create_cart_keyboard(cart_products: list[CartProduct]) -> InlineKeyboardMarkup:
@@ -9,4 +10,5 @@ async def create_cart_keyboard(cart_products: list[CartProduct]) -> InlineKeyboa
         [InlineKeyboardButton(f'Удалить из корзины "{cart_product.name}"', callback_data=cart_product.id)]
         for cart_product in cart_products
     ]
+    keyboard.append([SHOW_MENU_BUTTON])
     return InlineKeyboardMarkup(inline_keyboard=keyboard)
