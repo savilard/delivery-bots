73b4ef9b42becf79a9c4f6de7466ecf349b50fb4
savilard <vitaliyantonoff@gmail.com>
2021-06-08 12:39:34 +0600

    feat(tgbot): add sending delivery terms to customer

--- delivery_bots/bots/tgbot/checkout/location/handlers.py
+++ delivery_bots/bots/tgbot/checkout/location/handlers.py
@@ -1,13 +1,10 @@
 import httpx
 from aiogram import Dispatcher, types
-from aiogram.dispatcher import FSMContext
 from aiogram.utils.emoji import emojize
 
-from delivery_bots.api.moltin.entry.entry import (
-    fetch_all_entries,
-    parse_all_entries_response,
+from delivery_bots.bots.tgbot.checkout.location.messages import (
+    send_delivery_terms_to_customer,
 )
-from delivery_bots.bots.tgbot.checkout.location.location import find_nearest_pizzeria
 from delivery_bots.bots.tgbot.settings import YandexGeocoderApiSettings
 from delivery_bots.bots.tgbot.states import BotState
 
@@ -30,14 +27,17 @@ async def fetch_coordinates(geocoder_api_key: str, place: str):
         return most_relevant['GeoObject']['Point']['pos'].split(' ')
 
 
-async def handle_customer_location(message: types.Message, state: FSMContext):
+async def handle_customer_location(message: types.Message):
     """Processes the received coordinates of the customer."""
     location = message.location
-    await state.update_data(customer_lon=location.longitude)
-    await state.update_data(customer_lat=location.latitude)
+    await send_delivery_terms_to_customer(
+        message=message,
+        customer_lon=location.longitude,
+        customer_lat=location.latitude,
+    )
 
 
-async def handle_customer_address(message: types.Message, state: FSMContext):
+async def handle_customer_address(message: types.Message):
     """Processes the received address of the customer."""
     location = await fetch_coordinates(
         geocoder_api_key=YandexGeocoderApiSettings().api_key,
@@ -48,13 +48,8 @@ async def handle_customer_address(message: types.Message, state: FSMContext):
         await BotState.geo.set()
         return None
 
-    await state.update_data(customer_address=message.text)
-    await state.update_data(customer_lon=location[0])
-    await state.update_data(customer_lat=location[1])
-
-    entries = await parse_all_entries_response(await fetch_all_entries('pizzeria'))
-    await find_nearest_pizzeria(
-        entries=entries,
+    await send_delivery_terms_to_customer(
+        message=message,
         customer_lon=location[0],
         customer_lat=location[1],
     )
