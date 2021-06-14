b8e19dab7abb13eae4723d624ac1df2225ecf1d6
savilard <vitaliyantonoff@gmail.com>
2021-06-14 11:55:40 +0600

    refactor(tgbot): move getting cart content to separate func

--- delivery_bots/bots/tgbot/cart/messages.py
+++ delivery_bots/bots/tgbot/cart/messages.py
@@ -4,12 +4,8 @@ from delivery_bots.api.moltin.cart.schemas import CartProduct
 from delivery_bots.bots.tgbot.cart.keyboards import create_cart_keyboard
 
 
-async def display_cart(
-    query: CallbackQuery,
-    cart_products: list[CartProduct],
-    cart_total_amount: str,
-) -> None:
-    """Sends message with Moltin cart content."""
+async def make_cart_content_message(cart_products: list[CartProduct], cart_total_amount: str) -> str:
+    """Makes cart content message."""
     message_text = [
         '{name}\n{description}\n{quantity} пицц в корзине на сумму {amount}\n\n'.format(
             name=cart_product.name,
@@ -20,7 +16,16 @@ async def display_cart(
         for cart_product in cart_products
     ]
     message_text.append(f'К оплате {cart_total_amount}')
+    return ''.join(message_text)
+
+
+async def display_cart(
+    query: CallbackQuery,
+    cart_products: list[CartProduct],
+    message_text: str,
+) -> None:
+    """Sends message with Moltin cart content."""
     await query.message.answer(
-        text=''.join(message_text),
+        text=message_text,
         reply_markup=await create_cart_keyboard(cart_products),
     )
