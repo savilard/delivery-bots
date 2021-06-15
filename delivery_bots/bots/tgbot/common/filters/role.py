8c417ade94a9275097d7440d0aaa9c3bff51e06c
savilard <vitaliyantonoff@gmail.com>
2021-06-15 13:36:30 +0600

    feat(tgbot): add role filter

---
+++ delivery_bots/bots/tgbot/common/filters/role.py
@@ -0,0 +1,18 @@
+from aiogram.dispatcher.filters import BoundFilter
+from aiogram.types import CallbackQuery, Message
+
+from delivery_bots.bots.tgbot.settings import TgBotSettings
+
+
+class DeliveryManFilter(BoundFilter):
+    """Checks if the user is a delivery person."""
+
+    key = 'is_delivery_man'
+
+    async def check(self, message: Message = None, query: CallbackQuery = None):
+        """Checks from_user.id."""
+        tg_id_of_delivery_men = TgBotSettings().tg_id_of_delivery_men
+        if message:
+            return message.from_user.id in tg_id_of_delivery_men
+        if query:
+            return query.from_user.id in tg_id_of_delivery_men
