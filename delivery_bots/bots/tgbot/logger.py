9c095118136628b7ff179cca23dfa0e451bbedac
savilard <vitaliyantonoff@gmail.com>
2021-06-17 11:54:14 +0600

    deps(tgobt): remove loguru

--- delivery_bots/bots/tgbot/logger.py
+++ delivery_bots/bots/tgbot/logger.py
@@ -1,25 +1,6 @@
 import logging
 from typing import Union
 
-from loguru import logger
-
-
-class InterceptHandler(logging.Handler):
-    """Forwards standard logger messages to Loguru."""
-
-    def emit(self, record):  # noqa: D102
-        try:
-            level = logger.level(record.levelname).name
-        except ValueError:
-            level = record.levelno
-
-        frame, depth = logging.currentframe(), 2
-        while frame.f_code.co_filename == logging.__file__:  # noqa: WPS609
-            frame = frame.f_back  # type: ignore
-            depth += 1
-
-        logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())  # noqa: WPS221
-
 
 def configure_logging(level: Union[str, int] = 'DEBUG'):
     """
@@ -27,5 +8,4 @@ def configure_logging(level: Union[str, int] = 'DEBUG'):
 
     @param level: logging level, DEBUG by default
     """
-    logging.getLogger().handlers = [InterceptHandler()]
-    logging.getLevelName(level)
+    logging.basicConfig(level=level)
