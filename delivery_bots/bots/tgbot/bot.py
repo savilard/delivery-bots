1c221f67c47acd9714ea50578bba8b43db21097b
savilard <vitaliyantonoff@gmail.com>
2021-06-17 10:38:44 +0600

    refactor(tgbot): move handlres registration to __init__.py

--- delivery_bots/bots/tgbot/bot.py
+++ delivery_bots/bots/tgbot/bot.py
@@ -1,30 +1,13 @@
 from aiogram import Bot, Dispatcher, executor
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
 
-from delivery_bots.bots.tgbot.cart.handlers import register_handler_cart
-from delivery_bots.bots.tgbot.catalog_product_detail.handlers import (
-    register_catalog_product_detail_handler,
-)
-from delivery_bots.bots.tgbot.checkout.customer_contacts.handlers import (
-    register_handler_waiting_email,
-)
-from delivery_bots.bots.tgbot.checkout.delivery.handlers import (
-    register_handler_delivery,
-)
-from delivery_bots.bots.tgbot.checkout.delivery_man.handlers import (
-    register_handler_take_order_button,
-)
-from delivery_bots.bots.tgbot.checkout.location.handlers import (
-    register_handlers_location,
-)
+from delivery_bots.bots import register_handlers
 from delivery_bots.bots.tgbot.common.filters.role import (
     CustomerFilter,
     DeliveryManFilter,
 )
 from delivery_bots.bots.tgbot.logger import configure_logging
-from delivery_bots.bots.tgbot.menu.handlers import register_menu_handler
 from delivery_bots.bots.tgbot.settings import RedisSettings, TgBotSettings
-from delivery_bots.bots.tgbot.start.handlers import register_start_handler
 
 
 async def shutdown(dispatcher: Dispatcher):
@@ -49,14 +32,7 @@ if __name__ == '__main__':
 
     dp = Dispatcher(bot, storage=storage)
 
-    register_start_handler(dp)
-    register_menu_handler(dp)
-    register_catalog_product_detail_handler(dp)
-    register_handler_cart(dp)
-    register_handlers_location(dp)
-    register_handler_waiting_email(dp)
-    register_handler_delivery(dp)
-    register_handler_take_order_button(dp)
+    register_handlers(dp)
 
     dp.filters_factory.bind(DeliveryManFilter)
     dp.filters_factory.bind(CustomerFilter)
